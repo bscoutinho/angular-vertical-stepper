eb5efa1cefbbd04dd02b2480d9f3996c9706394b
DESKTOP-HVU60H0\Bruno Coutinho <bscoutinho2012@gmail.com>
2021-04-07 00:10:07 +0100

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,34 @@
-import { Component } from '@angular/core';
+import { animate, style, transition, trigger } from '@angular/animations';
+import { Component, OnInit } from '@angular/core';
+import {PrimeIcons} from 'primeng/api';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
+  animations: [
+    trigger('fadeInOut', [
+      transition(":enter", [
+        style({ opacity: 0 }),
+        animate(100, style({ opacity: 1 }))
+        ]),
+      transition(":leave", [
+        animate(100, style({ opacity: 0 }))
+        ])
+    ])
+  ]
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'angular-vertical-stepper';
+  sectionvisible: string = 'section1'
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+  openCloseTest(el) {
+    this.sectionvisible = el
+  }
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,21 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
 
 import { AppComponent } from './app.component';
+
+import { ButtonModule } from 'primeng/button';
+import { RippleModule } from 'primeng/ripple';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule, 
+    BrowserAnimationsModule,
+    ButtonModule,
+    RippleModule
   ],
   providers: [],
   bootstrap: [AppComponent]
